5d6628e0ebc383bf599e1ccdcba06d166fb7382e
ThiagoSGomes-Dev <thiagodos.10@gmail.com>
2026-02-09 19:37:26 -0300

    feat: Atualiza o Objeto de mock

--- app/src/main/java/br/com/android_mvp_intent_sample/mock/HandoutMockDataSource.kt
+++ app/src/main/java/br/com/android_mvp_intent_sample/mock/HandoutMockDataSource.kt
@@ -5,10 +5,10 @@ import br.com.android_mvp_intent_sample.HandOutModel
 object HandoutMockDataSource {
 
     fun getMock(): HandOutModel {
-        // simulando parse de JSON
+
         return HandOutModel (
             code = "VIP-777",
-            showRetention = true
+            showAttribute = true
         )
     }
 }
