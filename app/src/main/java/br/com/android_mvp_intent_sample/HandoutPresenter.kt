343791081129c0396243e4eda20088d3eb5c0c2a
ThiagoSGomes-Dev <thiagodos.10@gmail.com>
2026-02-09 19:36:56 -0300

    feat: Atualiza a presenter

--- app/src/main/java/br/com/android_mvp_intent_sample/HandoutPresenter.kt
+++ app/src/main/java/br/com/android_mvp_intent_sample/HandoutPresenter.kt
@@ -1,14 +1,21 @@
 package br.com.android_mvp_intent_sample
 
+import br.com.android_mvp_intent_sample.mock.HandoutMockDataSource
+
 class HandoutPresenter(
     private val view: HandoutContract.View
 ): HandoutContract.Presenter {
 
-    override fun onOpenHandoutClicked(code: String) {
-        val showAttribute = code.startsWith(prefix = "VIP")
-        view.navigateToHandout(
-            code = code,
-            showAttribute = showAttribute
+    override fun onOpenHandoutClicked(model: HandOutModel) {
+        val data = HandoutMockDataSource.getMock()
+
+        if (data.showAttribute) {
+            view.openRetention(data.code)
+        }
+
+        view.openHandout(
+            code = data.code,
+            showAttribute = data.showAttribute
         )
     }
 
